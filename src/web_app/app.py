20c47051f6a10b363957d1aecb72eddec547d800
Daron Marino <marino.daron@gmail.com>
2021-04-20 20:22:44 -0400

    accounting for any changes

--- src/web_app/app.py
+++ src/web_app/app.py
@@ -1,10 +1,25 @@
 from flask import Flask, render_template, url_for, request
 import numpy as np
+import pandas as pd
 from io import BytesIO
+
+# from  .. import itemrec
+
 
 
 app = Flask(__name__)
 
+# instatiating the df and fitting to the recommender
+artist_agg = pd.read_csv('../../spotify-data/artist_agg.csv')
+
+
+# recommender = itemrec.ItemRecommender()
+
+# recommender.fit(artist_agg)
+
+
+# building app
+@app.route('/')
 @app.route('/home', methods=['GET', 'POST'])
 def home(title='Music Recommender'):
   return render_template('home.html', title=title)
